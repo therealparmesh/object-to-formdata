234e6f4812061d9ac1b9130f8d732fd87b4e5157
shenyuanhai <syh@wrdtech.com>
2019-02-12 12:43:24 +0800

    add type declaration file.

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare function isUndefined(value: any): boolean;
+declare function isNull(value: any): boolean;
+declare function isObject(value: any): boolean;
+declare function isArray(value: any): boolean;
+declare function isDate(value: any): boolean;
+declare function isBlob(value: any): boolean;
+declare function isFile(value: any): boolean;
+declare function isFormData(value: any): boolean;
+declare type ParamType = undefined | null;
+interface ICfg {
+    indices?: boolean;
+    nulls?: boolean;
+}
+declare function objectToFormData(obj: any, cfg?: ICfg | FormData | ParamType, fd?: FormData, pre?: FormData | string): FormData;
